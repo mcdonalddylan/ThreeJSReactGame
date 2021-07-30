6c91ebd18581d65605293892fb2a65119b3ff77d
mcdonalddylan <soupaface@gmail.com>
2021-07-29 22:46:54 -0400

    kinda fixing the image slideshow

--- src/components/ImagesContainer.tsx
+++ src/components/ImagesContainer.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import './ImagesContainer.scss';
+import './HomePage.scss';
 
 interface IProps {
     images: string[],
@@ -11,13 +12,36 @@ interface IProps {
 export const ImagesContainer: React.FC<IProps> = (props:IProps) => {
 
     const [redirect, setRedirect] = useState(false);
+    const IMAGE_LOOP_TIMING_SECONDS = 3;
+
+    const loopImages = () => {
+        let counter = 0;
+        const run = setInterval(()=>{
+            console.log(counter);
+            if (counter === 0){
+                const previousImage = document.getElementById(`img${props.images.length-1}`);
+                if(previousImage) previousImage.style.display = 'none';
+                const currentImage = document.getElementById(`img${counter}`);
+                if (currentImage) currentImage.style.display = 'block';
+
+                counter++;
+            } else if (counter < props.images.length){
+
+                const previousImage = document.getElementById(`img${counter-1}`);
+                if(previousImage) previousImage.style.display = 'none';
+                const currentImage = document.getElementById(`img${counter}`);
+                if (currentImage) currentImage.style.display = 'block';
+
+                counter++;
+            } else {
+                clearInterval(run);
+            }
+        },IMAGE_LOOP_TIMING_SECONDS*1000);
+        counter = 0;
+    }
 
     useEffect(()=>{
-        setInterval(()=>{
-            props.images.forEach((img, ind) => {
-                const currentImg = document.getElementById(`img${ind}`);
-            })
-        },3500);
+        loopImages();
     });
 
     return (
@@ -28,24 +52,37 @@ export const ImagesContainer: React.FC<IProps> = (props:IProps) => {
                         <div className='img'>
                             {props.images.map((imgSrc, index)=>{
                                 console.log(imgSrc);
-                                return (index%2 === 0 ?
+                                return (
                                     <img key={index} id={`img${index}`} className='img-inner' style={{
                                         animationName: 'imageMoveDown',
-                                        animationDelay: `${index*5}s`,
+                                        animationTimingFunction: 'ease',
                                         animationIterationCount: 'infinite',
-                                        animationDuration: '5s',
+                                        animationDuration: `${IMAGE_LOOP_TIMING_SECONDS}s`,
                                         zIndex: index+1
                                     }}
                                     src={imgSrc} onClick={()=>setRedirect(true)}/>
-                                :
-                                    <img key={index} id={`img${index}`} className='img-inner' style={{
-                                        animationName: 'imageMoveUp',
-                                        animationDelay: `${index*5}s`,
-                                        animationIterationCount: 'infinite',
-                                        animationDuration: '5s',
-                                        zIndex: index+1
-                                    }}
-                                    src={imgSrc} onClick={()=>setRedirect(true)}/>)
+                                )
+                                // return (index%2 === 0 ?
+                                //     <div className='img-display' id={`display${index}`}>
+                                //         <img key={index} id={`img${index}`} className='img-inner' style={{
+                                //             animationName: 'imageMoveDown',
+                                //             animationDelay: `${index*5}s`,
+                                //             animationTimingFunction: 'ease',
+                                //             animationIterationCount: 'infinite',
+                                //             animationDuration: '3s',
+                                //             zIndex: index+1
+                                //         }}
+                                //         src={imgSrc} onClick={()=>setRedirect(true)}/>
+                                //     </div>
+                                // :
+                                //     <img key={index} id={`img${index}`} className='img-inner' style={{
+                                //         animationName: 'imageMoveUp',
+                                //         animationDelay: `${index*5}s`,
+                                //         animationIterationCount: 'infinite',
+                                //         animationDuration: '5s',
+                                //         zIndex: index+1
+                                //     }}
+                                //     src={imgSrc} onClick={()=>setRedirect(true)}/>)
                             })}
                         </div>
                     </div>
